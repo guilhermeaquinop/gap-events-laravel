1ea2fe61b41f5ae260d082cfba0961d15f4944f6
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2022-06-26 02:11:32 -0300

    Modificado campo da imagem para recibimento a partir do banco.

--- my-app/resources/views/welcome.blade.php
+++ my-app/resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
         
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
                 <p class="card-date">01/01/1999</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
