0afadfdbfac8de8fc54898ea029a64b4098caba3
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2022-07-04 00:31:44 -0300

    Projeto GAP Events finalizado.

--- my-app/app/Http/Controllers/EventController.php
+++ my-app/app/Http/Controllers/EventController.php
@@ -54,9 +54,7 @@ class EventController extends Controller
         }
 
         $user = auth()->user();
-        
         $event->user_id = $user->id;
-
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');   
@@ -66,18 +64,84 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+        
+        if ($user) {
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+            
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id){
+                    $hasUserJoined = true;
+                }
+            }
+        } 
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
 
     }
 
     public function dashboard() {
          
         $user = auth()->user();
-
         $events = $user->events;
+        $eventsAsParticipant = $user->eventsAsParticipant;
+        return view('events.dashboard', ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]);
+    }
 
-        return view('events.dashboard', ['events' => $events]);
+    public function destroy($id) {
+
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
+    }
+
+    public function edit($id) {
+
+        $user = auth()->user();
+
+        $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id){
+            return redirect('/dashboard');
+        }
+
+        return view('events.edit', ['event'=>$event]);
+    }
+
+    public function update(Request $request) {
+
+        $data = $request->all();
+        //Verifica se o arquivo é do tipo image
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            $requestImage = $request->image;
+            $extension = $requestImage->extension();
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . "." . $extension;
+            $requestImage->move(public_path('img/events'), $imageName);
+            $data['image'] = $imageName;
+        }
+    
+        Event::findOrFail($request->id)->update($data);
+
+        return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+    }
+
+    public function joinEvent($id) {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->attach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Presença no evento:' . $event->title . ', confirmada com sucesso!');
+    }
+
+    public function leaveEvent($id) {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', ' Remoção de presença no evento:' . $event->title . ', realizada com sucesso!');
     }
 }
--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -19,4 +19,15 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']); // store -> metodo de adição de daddos (padrão do laravel)
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
+
+
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
+
+
